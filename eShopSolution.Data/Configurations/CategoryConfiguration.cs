1c6dd4dcfb1e05579c50626aca5f6109ad0392c1
BinhNguyen <BinhNguyen@BINHNGUYEN>
2020-06-13 17:12:50 +0700

    Configure entity with Fluent API

---
+++ eShopSolution.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(c => c.id);
+            builder.Property(c => c.Status).HasDefaultValue(Status.Active);
+        }
+    }
+}
--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -9,8 +9,10 @@ namespace eShopSolution.Data.Entities
         public int id { get; set; }
         public decimal Price { get; set; }
         public decimal OriginalPrice { get; set; }
-        public int stock { get; set; }
+        public int Stock { get; set; }
         public int ViewCount { get; set; }
         public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
     }
 }
