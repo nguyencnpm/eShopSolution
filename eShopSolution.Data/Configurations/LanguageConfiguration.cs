46a0c24c93a1d2124331ff2a07f6ab7c62bc7921
BinhNguyen <BinhNguyen@BINHNGUYEN>
2020-06-15 00:43:20 +0700

    Configure entity with Fluent API

---
+++ eShopSolution.Data/Configurations/LanguageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class LanguageConfiguration : IEntityTypeConfiguration<Language>
+    {
+        public void Configure(EntityTypeBuilder<Language> builder)
+        {
+            builder.ToTable("Languages");
+
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.Id).IsRequired().IsUnicode(false).HasMaxLength(5);
+
+            builder.Property(l => l.Name).IsRequired().HasMaxLength(20);
+        }
+    }
+}
--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -14,5 +14,7 @@ namespace eShopSolution.Data.Entities
         public string SeoAlias { get; set; }
         public List<ProductInCategory> ProductInCategories { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }
